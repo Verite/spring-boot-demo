bf7bd5c0aed400dd98b529df702c170bc011dbd4
“Verite” <veriteli@aliyun.com>
2023-05-24 17:24:02 +0800

    feat(OpenFeign 使用 ):
    1，生产者建立
    2，消费者建立
    3，生产者和消费者的初级demo可以使用。

---
+++ demo-cloud-feign/demo-provider/src/main/java/com/xkcoding/provider/config/ProviderConstants.java
@@ -0,0 +1,13 @@
+package com.xkcoding.provider.config;
+
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Verite
+ * @date 2023/5/24 17:05
+ */
+@NoArgsConstructor
+public class ProviderConstants {
+
+   public static final String SYS_DESC = "——服务提供者1：";
+}
